d270422adcf8b35048be9eb926cc08c98a922106
ElYonathanD <yonathan123pr@gmail.com>
2024-12-06 22:49:49 -0500

    create global state with zustand

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,27 @@
+import { Container, Stack } from '@mui/material'
 import './App.css'
+import JsIcon from './components/JsIcon'
+import Start from './components/Start'
+import { useQuestionStore } from './store/questions'
 
 function App() {
+  const questions = useQuestionStore((state) => state.questions)
+  console.log(questions)
   return (
-    <>
-      <h1>JS Quiz</h1>
-    </>
+    <main>
+      <Container maxWidth='sm'>
+        <Stack
+          direction='row'
+          gap={4}
+          alignItems='center'
+          justifyContent='center'
+        >
+          <h1>JS Quiz</h1>
+          <JsIcon />
+        </Stack>
+        <Start />
+      </Container>
+    </main>
   )
 }
 
